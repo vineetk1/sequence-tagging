b918be483ca0162a4f5ba97edc89f3b70ceb3c1b
vineetk1 <vineetchandhok@gmail.com>
2022-10-26 10:25:44 -0400

    changes plus debugging

--- utilities.py
+++ utilities.py
@@ -9,36 +9,72 @@ from enum import Enum
 
 logg = getLogger(__name__)
 
-used_punctuations = ".,-$%"
-unused_punctuations = ""
-for punc in string.punctuation:
-    if punc not in used_punctuations:
-        unused_punctuations = f'{unused_punctuations}{punc}'
-del used_punctuations
-
 
 def preTokenize_splitWords(strng: str) -> List[str]:
     class Word_pos(Enum):
         BEGIN = 1       # position of char at beginning of word
-        MID = 2         # position of char at middle of word
-        MID_BEGIN = 3   # position of char at beginning segment of word
+        BEGIN_END = 2   # a single char in a word
+        MID = 3         # position of char at middle of word
+        MID_BEGIN = 4   # position of char at beginning segment of word
+        END = 5         # position of char at end of word
     word_pos = Word_pos.BEGIN
-    str2out_idx: List[int] = []
     word_idx: List[int] = []
-    idx_of_last_char = len(strng) - 1
+    strng2idx: List[int] = []
 
     for char_idx in range(len(strng)):
-        assert (not word_idx) if word_pos == Word_pos.BEGIN else word_idx
+
+        if strng[char_idx] == " ":
+            assert word_pos == Word_pos.BEGIN
+            assert not word_idx
+            continue
+        if char_idx == (len(strng) - 1) or strng[char_idx+1] == " ":
+            if word_pos == Word_pos.BEGIN:
+                word_pos = Word_pos.BEGIN_END
+            else:
+                word_pos = Word_pos.END
+        assert (not word_idx) if (word_pos == Word_pos.BEGIN or word_pos == Word_pos.BEGIN_END) else word_idx
+
         match strng[char_idx]:
-            case ",":   # comma
-                # begin: exclude, mid => end: exclude + new word
+            case char if char not in string.punctuation:
+                # begin->mid: include, begin_end->begin: include + new word,
+                # mid->mid: include, mid_begin->mid: include,
+                # end->begin: include + new word
                 match word_pos:
                     case Word_pos.MID:
-                        str2out_idx.append(word_idx.append(char_idx-1))
+                        pass
+                    case Word_pos.BEGIN | Word_pos.MID_BEGIN:
+                        word_idx.append(char_idx)
+                        word_pos = Word_pos.MID
+                    case Word_pos.END:
+                        # MID->END or MID_BEGIN->END
+                        if len(word_idx) % 2:
+                            word_idx.append(char_idx)
+                        else:
+                            word_idx.append([char_idx, char_idx])
+                        strng2idx.append(word_idx)
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case Word_pos.BEGIN_END:
+                        strng2idx.append([char_idx, char_idx])
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case _:
+                        assert False
+            case ",":   # comma
+                # begin->begin: exclude, begin_end->begin: exclude,
+                # mid->begin: exclude + new word,
+                # mid_begin->begin: exclude + new word,
+                # end->begin: exclude + new word
+                match word_pos:
+                    case Word_pos.END | Word_pos.MID:
+                        word_idx.append(char_idx-1)
+                        strng2idx.append(word_idx)
                     case Word_pos.MID_BEGIN:
-                        str2out_idx.append(word_idx)
+                        strng2idx.append(word_idx)
                     case Word_pos.BEGIN:
-                        assert not word_idx
+                        pass
+                    case Word_pos.BEGIN_END:
+                        pass
                     case _:
                         assert False
                 word_pos = Word_pos.BEGIN
@@ -46,30 +82,44 @@ def preTokenize_splitWords(strng: str) -> List[str]:
             case ".":   # period
                 pass
             case "-":   # hypen
-                # begin: exclude, mid: include, end: exclude
+                # begin->begin: exclude, begin_end->begin: exclude,
+                # mid->mid: include, mid_begin->mid: include,
+                # end->begin: exclude + new word
                 match word_pos:
                     case Word_pos.MID:
-                        if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                            str2out_idx.append(word_idx.append(char_idx-1))
-                            word_pos = Word_pos.BEGIN
-                            word_idx = []
+                        pass
                     case Word_pos.MID_BEGIN:
-                        if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                            str2out_idx.append(word_idx)
-                            word_pos = Word_pos.BEGIN
-                            word_idx = []
-                        else:
-                            word_idx.append(char_idx)
-                            word_pos = Word_pos.MID
+                        word_idx.append(char_idx)
+                        word_pos = Word_pos.MID
                     case Word_pos.BEGIN:
-                        assert not word_idx
+                        pass
+                    case Word_pos.BEGIN_END:
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case Word_pos.END:
+                        word_idx.append(char_idx-1)
+                        strng2idx.append(word_idx)
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
                     case _:
                         assert False
             case "$":   # dollar
-                # begin: new word, mid: exclude, end: exclude
+                # begin->begin: include + new word,
+                # begin_end->begin: include + new word,
+                # mid->mid_begin: exclude, mid_begin->mid_begin: exclude,
+                # end->begin: exclude + new word
                 match word_pos:
                     case Word_pos.BEGIN:
-                        str2out_idx.append([char_idx, char_idx])
+                        strng2idx.append([char_idx, char_idx])
+                    case Word_pos.BEGIN_END:
+                        strng2idx.append([char_idx, char_idx])
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case Word_pos.END:
+                        word_idx.append(char_idx-1)
+                        strng2idx.append(word_idx)
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
                     case Word_pos.MID:
                         word_idx.append(char_idx-1)
                         word_pos = Word_pos.MID_BEGIN
@@ -78,117 +128,71 @@ def preTokenize_splitWords(strng: str) -> List[str]:
                     case _:
                         assert False
             case "%":   # percent
-                # begin: exclude, mid: exclude, end: include + two words
+                # begin->begin: exclude, begin_end->begin: exclude,
+                # mid->mid_begin: exclude, mid_begin->mid_begin: exclude,
+                # end->begin: include + two words
                 match word_pos:
+                    case Word_pos.END:
+                        word_idx.append(char_idx-1)
+                        strng2idx.append(word_idx)
+                        strng2idx.append([char_idx, char_idx])
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
                     case Word_pos.MID:
-                        if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                            str2out_idx.append(word_idx.append(char_idx-1))
-                            str2out_idx.append([char_idx, char_idx])
-                            word_pos = Word_pos.BEGIN
-                            word_idx = []
-                        else:
-                            word_idx.append(char_idx-1)
-                            word_pos = Word_pos.MID_BEGIN
+                        word_idx.append(char_idx-1)
+                        word_pos = Word_pos.MID_BEGIN
                     case Word_pos.MID_BEGIN:
-                        if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                            str2out_idx.append(word_idx)
-                            str2out_idx.append([char_idx, char_idx])
-                            word_pos = Word_pos.BEGIN
-                            word_idx = []
+                        pass
                     case Word_pos.BEGIN:
-                        if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                            str2out_idx.append([char_idx, char_idx])
-                        else:
-                            assert not word_idx
+                        pass
+                    case Word_pos.BEGIN_END:
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
                     case _:
                         assert False
-            case " ":   # space
-                match word_pos:
-                    case Word_pos.MID:
-                        assert word_idx
-                        str2out_idx.append(word_idx.append(char_idx-1))
-                    case Word_pos.BEGIN:
-                        assert not word_idx
-                    case Word_pos.MID_BEGIN:
-                        assert word_idx
-                        str2out_idx.append(word_idx)
-                    case _:
-                        assert False
-                word_pos = Word_pos.BEGIN
-                word_idx = []
             case _:
-                if strng[char_idx] not in unused_punctuations:
-                    # begin: include, mid: include, end: include
-                    match word_pos:
-                        case Word_pos.MID:
-                            pass
-                        case Word_pos.BEGIN | Word_pos.MID_BEGIN:
-                            assert (not word_idx) if word_pos == Word_pos.BEGIN else word_idx
-                            word_idx.append(char_idx)
-                            word_pos = Word_pos.MID
-                        case _:
-                            assert False
-                else:
-                    # begin: exclude, mid: exclude, end: exclude + new word
-                    match word_pos:
-                        case Word_pos.MID:
-                            if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                                str2out_idx.append(word_idx.append(char_idx-1))
-                                word_pos = Word_pos.BEGIN
-                                word_idx = []
-                            else:
-                                word_idx.append(char_idx-1)
-                                word_pos = Word_pos.MID_BEGIN
-                        case Word_pos.MID_BEGIN:
-                            if char_idx == idx_of_last_char or strng[char_idx+1] == " ":
-                                str2out_idx.append(word_idx)
-                                word_pos = Word_pos.BEGIN
-                                word_idx = []
-                        case Word_pos.BEGIN:
-                            pass
-                        case _:
-                            assert False
+                # begin->begin: exclude, begin_end->begin: exclude,
+                # mid->mid_begin: exclude, mid_begin->mid_begin: exclude,
+                # end->begin: exclude + new words
+                match word_pos:
+                    case Word_pos.END:
+                        word_idx.append(char_idx-1)
+                        strng2idx.append(word_idx)
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case Word_pos.MID:
+                        word_idx.append(char_idx-1)
+                        word_pos = Word_pos.MID_BEGIN
+                    case Word_pos.MID_BEGIN:
+                        pass
+                    case Word_pos.BEGIN:
+                        pass
+                    case Word_pos.BEGIN_END:
+                        word_pos = Word_pos.BEGIN
+                        word_idx = []
+                    case _:
+                        assert False
 
-    match word_pos:
-        case Word_pos.BEGIN | Word_pos.MID_BEGIN:
-            assert not word_idx
-        case Word_pos.MID:
-            str2out_idx.append(word_idx.append(char_idx-1))
-        case _:
-            assert False
+    assert word_pos == Word_pos.BEGIN
+    assert not word_idx
+    assert (len(strng2idx) % 2) == 0
 
-    assert (len(str2out_idx) % 2) == 0
     strng_pretok_split = []
-    for word_idxs in str2out_idx:
-        if len(word_idxs) == 2:
-            strng_pretok_split.append(strng[word_idxs[0]: word_idxs[1]+1])
-        else:
-            word = ""
-            for word_idx in range(0, len(word_idxs), 2):
-                word = f'{word}{strng[word_idxs[word_idx]: word_idxs[word_idx]+1]}'
-            strng_pretok_split.append(word)
+    for word_idx in strng2idx:
+        match len(word_idx):
+            case 2:
+                #???? What if word_idx[1] is at end of string; then word_idx[1]+1 gives index error
+                strng_pretok_split.append(strng[word_idx[0]: word_idx[1]+1])
+            case 4:
+                strng_pretok_split.append(strng[word_idx[0]: word_idx[1]+1])
+                strng_pretok_split.append(strng[word_idx[2]: word_idx[3]+1])
+            case _:
+                assert (len(word_idx) % 2) == 0
+                word = ""
+                for idx in range(0, len(word_idx), 2):
+                    word = f'{word}{strng[word_idx[idx]: word_idx[idx]+1]}'
+                strng_pretok_split.append(word)
     return strng_pretok_split
-
-
-
-
-
-def xpreTokenize_splitWords(strng: str) -> List[str]:
-    # *****NOTE**** The best way to implement is to pick one character at a time and decide what to do; also if a character is at the end of a word then it could be treated differently than when it is in the middle of the word; for example: user types "red,blue"then the comma should be replaced with space
-
-    # examples: "$400.56." -> ['$', '400.56']   "100%" -> ['100', '%']
-    #           "mer#ce.des!" -> ['mer', 'ce.des']   "let's" -> ['let', 's']
-    new_strng: List[str] = (strng.translate(table)).split()
-    final_strng = []
-    for word in new_strng:
-        # period and decimal-point have the same punctuation mark; remove
-        # periods from words that end in periods, which will not include
-        # decimal-points because they are not at the end of words
-        if word.endswith("."):
-            final_strng.append(word.strip("."))
-        else:
-            final_strng.append(word)
-    return final_strng
 
 
 def convert_tokenLabels2wordLabels(tokenizer, user_input_pretok: List[str],
@@ -226,7 +230,8 @@ def generate_user_output(user_input_pretok: List[str], word_labels: List[str]):
 def generate_history(user_output: str) -> str:
     return (" ")
 
-strng = ",.-$% $44.#6$7$ re%d,g$een,$20.4 dark#$-br\o>w?:n -n-o--"
+
+strng = ",.-$% $44.#6$7$ re%d,g$een,$20.4 dark#$-br\t>w?:n -n-o--"
 print(preTokenize_splitWords(strng))
 print(preTokenize_splitWords(""))
 print(preTokenize_splitWords(","))
