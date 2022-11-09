cbe44d4a64922a9b497f8eafa8fd664d0a5292ab
vineetk1 <vineetchandhok@gmail.com>
2022-11-09 10:48:38 -0500

    splits the dataset into train/val/test and collects metadata

---
+++ Split_dataset.py
@@ -0,0 +1,107 @@
+'''
+Vineet Kumar, sioom.ai
+'''
+
+from logging import getLogger
+from typing import List, Dict, Tuple, Any
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pathlib
+import pickle
+from collections import Counter
+
+logg = getLogger(__name__)
+
+
+def split_dataset(
+    dataset_path: str, splits: Dict[str, int], batch_sizes: Dict[str, int]
+) -> Tuple[Dict[str, Any], List[List[List[Any]]], List[List[List[Any]]],
+           List[List[List[Any]]]]:
+    assert splits['train'] + splits['val'] + splits['test'] == 100
+
+    # retrieve data files
+    dirName = pathlib.Path(dataset_path).resolve(strict=True).parents[0]
+    fileName_noSuffix = pathlib.Path(dataset_path).stem
+    dataset_file = dirName.joinpath(f'{fileName_noSuffix}.df')
+    dataset_meta_file = dirName.joinpath(f'{fileName_noSuffix}.meta')
+    if (not dataset_file.exists()) or (not dataset_meta_file.exists()):
+        strng = ('Either one or both of following files do not exist: '
+                 '{dataset_file}, {dataset_meta_file}')
+        logg.critical(strng)
+        exit()
+    df = pd.read_pickle(dataset_file)
+    with dataset_meta_file.open('rb') as dmF:
+        bio2_label_names = pickle.load(dmF)
+
+    # Split dataset into train, val, test
+    if not splits['train'] and splits['test']:
+        # testing a dataset on a checkpoint file; no training
+        df_train, df_val, df_test, splits['val'], splits[
+            'test'] = None, None, df, 0, 100
+    else:
+        df_train, df_temp = train_test_split(df,
+                                             shuffle=True,
+                                             stratify=None,
+                                             train_size=(splits['train'] /
+                                                         100),
+                                             random_state=42)
+        df_val, df_test = train_test_split(
+            df_temp,
+            shuffle=True,
+            stratify=None,
+            test_size=(splits['test'] / (splits['val'] + splits['test'])),
+            random_state=42)
+        assert len(df) == len(df_train) + len(df_val) + len(df_test)
+
+    train_data, val_data, test_data = (df_train[[
+        'dlg id', 'turn num', 'user input', 'history', 'token labels nums'
+    ]].values.tolist() if df_train is not None else None, df_val[[
+        'dlg id', 'turn num', 'user input', 'history', 'token labels nums'
+    ]].values.tolist() if df_val is not None else None, df_test[[
+        'dlg id', 'turn num', 'user input', 'history', 'token labels nums'
+    ]].values.tolist() if df_test is not None else None)
+
+    # create meta-data for the datasets
+    def token_labels_count(dataset):
+        if dataset is None:
+            return None
+        count = Counter()
+        for example in dataset:
+            for token_label in example[4]:
+                count[token_label] += 1
+        return dict(count)
+
+    trainValTest_tokenLabels_count = [
+        token_labels_count(dataset)
+        for dataset in (train_data, val_data, test_data)
+    ]
+
+    # e.g. 'test-set unseen token-labels': token-labels in test-dataset that
+    # are not in train-dataset
+    trainValTest_tokenLabels_unseen = [
+        set(range(len(bio2_label_names))).difference(
+            set(sub_dataset_tokenLabels_count.keys()))
+        for sub_dataset_tokenLabels_count in (trainValTest_tokenLabels_count)
+    ]
+
+    dataset_metadata = {
+        'batch sizes': batch_sizes,
+        'dataset splits': splits,
+        'dataset lengths': {
+            'original': len(df),
+            'train': len(df_train) if df_train is not None else 0,
+            'val': len(df_val) if df_val is not None else 0,
+            'test': len(df_test) if df_test is not None else 0
+        },
+        'token-label names': bio2_label_names,
+        'train token-labels -> number:count':
+        trainValTest_tokenLabels_count[0],
+        'val token-labels -> number:count': trainValTest_tokenLabels_count[1],
+        'test token-labels -> number:count': trainValTest_tokenLabels_count[2],
+        'train unseen token-labels': trainValTest_tokenLabels_unseen[0],
+        'val unseen token-labels': trainValTest_tokenLabels_unseen[1],
+        'test unseen token-labels': trainValTest_tokenLabels_unseen[2],
+        'dataset_panda': dataset_file
+    }
+
+    return dataset_metadata, train_data, val_data, test_data
