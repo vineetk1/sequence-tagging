a614ca6ba6cd4882aff5a97164ffdb95a76c8c0f
vineetk1 <vineetchandhok@gmail.com>
2022-11-25 16:52:06 -0500

    lot of additions and changes

--- Utilities.py
+++ Utilities.py
@@ -7,6 +7,7 @@ from typing import List, Dict, Tuple, Any
 import string
 from enum import Enum
 import torch
+import data.synthetic_dataset as syntheticData
 
 logg = getLogger(__name__)
 
@@ -75,24 +76,29 @@ def preTokenize_splitWords(strng: str) -> List[str]:
                         assert False
             case ",":   # comma
                 # begin (exclude) begin;  begin2end (exclude) begin;
-                # mid (exclude + new word) begin;
+                # mid (exclude) mid_begin;
                 # mid2end (exclude + new word) begin;
-                # mid_begin (exclude + new word) begin;
+                # mid_begin (exclude) mid_begin;
                 # mid_begin2end (exclude + new word) begin;
                 match charPos_inStrngWord:
                     case CharPos_inStrngWord.MID2END:
                         strngWord2Idx.append(char_idx-1)
                         strng2idx.append(strngWord2Idx)
+                        charPos_inStrngWord = CharPos_inStrngWord.BEGIN
+                        strngWord2Idx = []
                     case CharPos_inStrngWord.MID:
                         strngWord2Idx.append(char_idx-1)
-                        strng2idx.append(strngWord2Idx)
+                        charPos_inStrngWord = CharPos_inStrngWord.MID_BEGIN
                     case CharPos_inStrngWord.MID_BEGIN2END:
                         strng2idx.append(strngWord2Idx)
+                        charPos_inStrngWord = CharPos_inStrngWord.BEGIN
+                        strngWord2Idx = []
                     case CharPos_inStrngWord.MID_BEGIN:
-                        strng2idx.append(strngWord2Idx)
+                        pass
                     case (CharPos_inStrngWord.BEGIN |
                           CharPos_inStrngWord.BEGIN2END):
-                        pass
+                        charPos_inStrngWord = CharPos_inStrngWord.BEGIN
+                        strngWord2Idx = []
                     case _:
                         assert False
                 charPos_inStrngWord = CharPos_inStrngWord.BEGIN
@@ -231,80 +237,133 @@ def preTokenize_splitWords(strng: str) -> List[str]:
 
 
 def convert_tokenLabels2wordLabels(
-        tokenizer, batch: Dict[str, Any],
-        batch_nnOut_tokenLabels_ids: torch.Tensor,
-        all_token_labels: List[str]) -> List[List[str]]:
-    batch_word_labels = []
-    # tokens between two SEP belong to tokens of batch['user_input_pretok']
-    userInputTokens_beginEnd_idx = (
-            batch['nnIn_ids']['input_ids'] == 102).nonzero()
+        bch: Dict[str, Any],
+        bch_nnOut_tokenLabels_ids: torch.Tensor,
+        idx2tokenLabels: List[str]) -> List[List[str]]:
+    bch_wordLabels = []
+    # tokens between two SEP belong to tokens of bch['userIn_pretok']
+    userInTokens_beginEnd_idx = (
+            bch['nnIn_ids']['input_ids'] == 102).nonzero()
 
-    for bch_idx in range(batch_nnOut_tokenLabels_ids.shape[0]):
+    for bch_idx in range(bch_nnOut_tokenLabels_ids.shape[0]):
         prev_word_idx = None
-        word_labels = []
-        for userInputToken_idx in range(
-                (userInputTokens_beginEnd_idx[bch_idx * 2, 1] + 1).item(), (
-                   userInputTokens_beginEnd_idx[(bch_idx * 2) + 1, 1]).item()):
-            word_idx = batch['mapWords2Tokens'][bch_idx][userInputToken_idx]
+        wordLabels = []
+        for userInToken_idx in range(
+                (userInTokens_beginEnd_idx[bch_idx * 2, 1] + 1).item(), (
+                   userInTokens_beginEnd_idx[(bch_idx * 2) + 1, 1]).item()):
+            word_idx = bch['mapWords2Tokens'][bch_idx][userInToken_idx]
             if word_idx != prev_word_idx:  # first token of a word
-                word_labels.append(all_token_labels[
-                    batch_nnOut_tokenLabels_ids[
-                        bch_idx, userInputToken_idx].item()])
+                wordLabels.append(idx2tokenLabels[
+                    bch_nnOut_tokenLabels_ids[
+                        bch_idx, userInToken_idx].item()])
             prev_word_idx = word_idx
-        batch_word_labels.append(word_labels)
-    return (batch_word_labels)
+        bch_wordLabels.append(wordLabels)
+    return (bch_wordLabels)
 
 
 def CHECK_convert_tokenLabels2wordLabels(
-        tokenizer, batch: Dict[str, Any],
-        batch_nnOut_tokenLabels_ids: torch.Tensor,
-        all_token_labels: List[str]) -> List[List[str]]:
+        bch: Dict[str, Any],
+        bch_nnOut_tokenLabels_ids: torch.Tensor,
+        idx2tokenLabels: List[str]) -> List[List[str]]:
     # purpose of this function is to check that word-labels are generated
     # correctly
-    assert batch['nnIn_ids'][
-            'input_ids'].shape == batch_nnOut_tokenLabels_ids.shape
-    batch_word_labels = []
-    # tokens between two SEP belong to tokens of batch['user_input_pretok']
-    userInputTokens_beginEnd_idx = (
-            batch['nnIn_ids']['input_ids'] == 102).nonzero()
+    assert bch['nnIn_ids'][
+            'input_ids'].shape == bch_nnOut_tokenLabels_ids.shape
+    bch_wordLabels = []
+    # tokens between two SEP belong to tokens of bch['userIn_pretok']
+    userInTokens_beginEnd_idx = (
+            bch['nnIn_ids']['input_ids'] == 102).nonzero()
 
-    for bch_idx in range(batch_nnOut_tokenLabels_ids.shape[0]):
+    for bch_idx in range(bch_nnOut_tokenLabels_ids.shape[0]):
         prev_word_idx = None
-        word_labels = []
-        for userInputToken_idx in range(
-                (userInputTokens_beginEnd_idx[bch_idx * 2, 1] + 1).item(), (
-                   userInputTokens_beginEnd_idx[(bch_idx * 2) + 1, 1]).item()):
-            word_idx = batch['mapWords2Tokens'][bch_idx][userInputToken_idx]
+        wordLabels = []
+        for userInToken_idx in range(
+                (userInTokens_beginEnd_idx[bch_idx * 2, 1] + 1).item(), (
+                   userInTokens_beginEnd_idx[(bch_idx * 2) + 1, 1]).item()):
+            word_idx = bch['mapWords2Tokens'][bch_idx][userInToken_idx]
             assert word_idx is not None
-            batch_nnOut_tokenLabel = all_token_labels[
-                    batch_nnOut_tokenLabels_ids[
-                        bch_idx, userInputToken_idx].item()]
+            bch_nnOut_tokenLabel = idx2tokenLabels[
+                    bch_nnOut_tokenLabels_ids[
+                        bch_idx, userInToken_idx].item()]
             if word_idx != prev_word_idx:  # first token of a word
-                word_labels.append(batch_nnOut_tokenLabel)
+                wordLabels.append(bch_nnOut_tokenLabel)
                 # checking for correctness
                 # the first token can also be "I"; for example:
                 # color = “dark brown”, word-label = B-color, I-color
                 remaining_tokenLabel_of_word = (
-                        f"I{batch_nnOut_tokenLabel[1:]}" if (
-                            batch_nnOut_tokenLabel[0]) != "O" else "O")
+                        f"I{bch_nnOut_tokenLabel[1:]}" if (
+                            bch_nnOut_tokenLabel[0]) != "O" else "O")
             elif word_idx == prev_word_idx:  # not first token of a word
                 #*******Since token_labels are compared in Model.predict_step(), is it necessary to do it again here????
-                #assert batch_nnOut_tokenLabel == remaining_tokenLabel_of_word
+                #assert bch_nnOut_tokenLabel == remaining_tokenLabel_of_word
                 pass
             else:
                 assert False
             prev_word_idx = word_idx
-        batch_word_labels.append(word_labels)
-    assert len(batch['user_input_pretok']) == len(batch_word_labels)
-    for batch_idx in range(len(batch_word_labels)):
-        assert len(batch['user_input_pretok'][batch_idx]) == len(
-                batch_word_labels[batch_idx])
-    return (batch_word_labels)
+        bch_wordLabels.append(wordLabels)
+    assert len(bch['userIn_pretok']) == len(bch_wordLabels)
+    for bch_idx in range(len(bch_wordLabels)):
+        assert len(bch['userIn_pretok'][bch_idx]) == len(
+                bch_wordLabels[bch_idx])
+    return (bch_wordLabels)
 
 
-def generate_userOutputs_histories(user_input_pretok: List[str], word_labels:
-                                   List[str]) -> Tuple[str, List[str]]:
-    assert len(word_labels) == len(user_input_pretok)
-    user_output = " "
-    history = [" "]
-    return user_output, history
+def userOut_init():
+    userOut = {}
+    for car_entity_name in syntheticData.groupOf_car_entity_names:
+        # dict keys are in same order as syntheticData.groupOf_car_entity_names
+        userOut[car_entity_name] = []
+    return userOut
+
+
+def generate_userOut(
+        bch_prev_userOut: List[Dict[str, List[str]]],
+        bch_userIn_pretok: List[List[str]],
+        bch_wordLabels: List[List[str]]) -> List[Dict[str, List[str]]]:
+    assert len(bch_wordLabels) == len(bch_userIn_pretok)
+    assert len(bch_prev_userOut) == len(bch_userIn_pretok)
+    # init_userOut = userOut_init(); bch_userOut =
+    # [init_userOut for _ in range(len(bch_wordLabels))] Does NOT work
+    # because each copy of dict points to same memory location; i.e. writing a
+    # value to a key in a dict will write that value to all dicts
+    bch_userOut = [userOut_init() for _ in range(len(bch_wordLabels))]
+
+    for bch_idx in range(len(bch_wordLabels)):
+        tkn_lbl_idx = 0
+        while tkn_lbl_idx < len(bch_wordLabels[bch_idx]):
+
+            def _extract_from_tkn_lbl(label):
+                label_prefix = label[0]     # 'B' or 'I'
+                if label[-1] == ')':
+                    try:
+                        label_openParen_idx = label.index('(')
+                    except ValueError:
+                        logg.critical(
+                          '{label} has closing- but not opening-parenthesis')
+                        exit()
+                    label_name = label[2: label_openParen_idx]
+                    label_value = label[label_openParen_idx+1: -1]
+                else:
+                    label_name = label[2:]
+                    label_value = None
+                return label_prefix, label_name, label_value
+            tkn_lbl_prefix, tkn_lbl_name, tkn_lbl_value = _extract_from_tkn_lbl(
+                    bch_wordLabels[bch_idx][tkn_lbl_idx])
+
+            match tkn_lbl_name:
+                case tkn_lbl_name if (tkn_lbl_name in syntheticData.groupOf_car_entity_names_with_str_entity_values):
+                    # neural-net must memorize value (eg. mercedes) with its
+                    # corresponding name (eg. brand)
+                    bch_userOut[bch_idx][tkn_lbl_name].append(tkn_lbl_value if tkn_lbl_value  else bch_userIn_pretok[bch_idx][tkn_lbl_idx])
+                case tkn_lbl_name if (tkn_lbl_name in syntheticData.groupOf_car_entity_names_with_floatInt_entity_values) or (tkn_lbl_name in syntheticData.groupOf_car_entity_names_with_int_entity_values):
+                    assert tkn_lbl_value is None
+                    bch_userOut[bch_idx][tkn_lbl_name].append(bch_userIn_pretok[bch_idx][tkn_lbl_idx])
+
+            tkn_lbl_idx += 1
+
+    return bch_userOut
+
+
+def userOut2history(
+        bch_userOut: List[Dict[str, List[str]]]) -> List[List[str]]:
+    x = 0
