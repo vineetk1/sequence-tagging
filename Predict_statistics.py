1cb66123921b1e3efe9da67aa090f2b40e838bdc
vineetk1 <vineetchandhok@gmail.com>
2023-03-25 22:50:31 -0400

    changed names and add another file to open

--- Predict_statistics.py
+++ Predict_statistics.py
@@ -19,23 +19,24 @@ def failed_nnOut_tknLblIds(
     bch: Dict[str, Any],
     bch_nnOut_tknLblIds: torch.Tensor,
     bch_userIn_filtered_entityWrds: List[Union[List[str], None]],
-    bch_nnOut_entityWrdLbls: List[Union[List[str], None]],
+    bch_nnOut_entityLbls: List[Union[List[str], None]],
     bch_userOut: List[Dict[str, List[str]]],
     df: pd.DataFrame,
     tokenizer,
     dataset_meta: Dict[str, Any],
-    failed_nnOut_tknLblIds: pathlib.Path,
+    failed_nnOut_tknLblIds_file: pathlib.Path,
+    passed_nnOut_tknLblIds_file: pathlib.Path,
 ) -> Tuple[int, int, int, int, int, Dict[str, int]]:
     count_total_turns: int = 0
     count_failed_nnOut_tknLblIds: int = 0
-    count_failedTurns_nnOut_entityWrdLbl: int = 0
+    count_failedTurns_nnOut_entityLbl: int = 0
     count_failedTurns_nnOut_userOut: int = 0
     count_failed_turns: int = 0
     counter_failed_nnOut_tknLblIds: Counter(Counter, Dict[str,
                                                           int]) = Counter()
 
-    assert bch_nnOut_tknLblIds.shape[0] == len(bch_nnOut_entityWrdLbls)
-    count_total_turns += len(bch_nnOut_entityWrdLbls)
+    assert bch_nnOut_tknLblIds.shape[0] == len(bch_nnOut_entityLbls)
+    count_total_turns += len(bch_nnOut_entityLbls)
     bch_nnOut_tknLblIds = torch.where(bch['tknLblIds'] == -100,
                                       bch['tknLblIds'], bch_nnOut_tknLblIds)
     failed_bchIdxs_nnOutTknLblIdIdxs: List[List[int, int]] = torch.ne(
@@ -46,32 +47,23 @@ def failed_nnOut_tknLblIds(
         for failed_bchIdx_nnOutTknLblIdIdx in failed_bchIdxs_nnOutTknLblIdIdxs
     }
 
-    bch_entityWrdLbls_True: List[List[str]] = []
-    bch_failed_nnOut_entityWrdLbls: List[List[str]] = []
+    bch_entityLbls_True: List[List[str]] = []
+    bch_failed_nnOut_entityLbls: List[List[str]] = []
     for bch_idx, (dlgId, trnId) in enumerate(bch['dlgTrnId']):
-        bch_entityWrdLbls_True.append([])
-        bch_failed_nnOut_entityWrdLbls.append([])
-        # ******Finally, must be able to retrieve entityWrdLbls_True from df instead of wrdLbls_True*****
-        wrdLbls_True: List[str] = (
+        bch_failed_nnOut_entityLbls.append([])
+        bch_entityLbls_True.append(
             df[(df['dlgId'] == dlgId)
-               & (df['trnId'] == trnId)]['wrdLbls']).item()
-        for wrdLbl_True in wrdLbls_True:
-            if wrdLbl_True[0] == 'B' or wrdLbl_True[0] == 'I':
-                if wrdLbl_True[-1] == ')':
-                    bch_entityWrdLbls_True[-1].append(
-                        wrdLbl_True[2:wrdLbl_True.index('(')])
-                else:
-                    bch_entityWrdLbls_True[-1].append(wrdLbl_True[2:])
-        if bch_nnOut_entityWrdLbls[bch_idx] != bch_entityWrdLbls_True[-1]:
-            count_failedTurns_nnOut_entityWrdLbl += 1
-            for entityWrdLbl_True, nnOut_entityWrdLbl in zip_longest(
-                    bch_entityWrdLbls_True[-1],
-                (bch_nnOut_entityWrdLbls[bch_idx]
-                 if bch_nnOut_entityWrdLbls[bch_idx] is not None else [])):
-                if entityWrdLbl_True != nnOut_entityWrdLbl:
-                    bch_failed_nnOut_entityWrdLbls[-1].append(
-                        f"({entityWrdLbl_True}, {nnOut_entityWrdLbl})")
-    assert len(bch_entityWrdLbls_True) == len(bch_nnOut_entityWrdLbls)
+               & (df['trnId'] == trnId)]['entityLbls'].item())
+        if bch_nnOut_entityLbls[bch_idx] != bch_entityLbls_True[-1]:
+            count_failedTurns_nnOut_entityLbl += 1
+            for entityLbl_True, nnOut_entityLbl in zip_longest(
+                    bch_entityLbls_True[-1],
+                (bch_nnOut_entityLbls[bch_idx]
+                 if bch_nnOut_entityLbls[bch_idx] is not None else [])):
+                if entityLbl_True != nnOut_entityLbl:
+                    bch_failed_nnOut_entityLbls[-1].append(
+                        f"({entityLbl_True}, {nnOut_entityLbl})")
+    assert len(bch_entityLbls_True) == len(bch_nnOut_entityLbls)
 
     bch_userOut_True: List[Dict[str, List[str]]] = []
     bch_failed_nnOut_userOut: List[Union[Dict[str, List[str]], None]] = []
@@ -93,33 +85,33 @@ def failed_nnOut_tknLblIds(
             bch_failed_nnOut_userOut.append(str(d))
 
     # inner-lists must have same bch_idx occuring consectively
-    failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut: List[List[
+    failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut: List[List[
         int, int]] = []
-    for bch_idx in range(len(bch_entityWrdLbls_True)):
+    for bch_idx in range(len(bch_entityLbls_True)):
         if bch_idx in failed_bchIdxs:
             for failed_bchIdx_nnOutTknLblIdIdx in (
                     failed_bchIdxs_nnOutTknLblIdIdxs):
                 if failed_bchIdx_nnOutTknLblIdIdx[0] == bch_idx:
-                    failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut.append(
+                    failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut.append(
                         failed_bchIdx_nnOutTknLblIdIdx)
-        elif bch_failed_nnOut_entityWrdLbls[bch_idx]:
-            failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut.append(
+        elif bch_failed_nnOut_entityLbls[bch_idx]:
+            failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut.append(
                 [bch_idx, None])
         elif bch_failed_nnOut_userOut[bch_idx] is not None:
-            failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut.append(
+            failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut.append(
                 [bch_idx, None])
         else:
             pass
 
-    if not failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut:
+    if not failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut:
         return (count_total_turns, count_failed_nnOut_tknLblIds,
-                count_failedTurns_nnOut_entityWrdLbl,
+                count_failedTurns_nnOut_entityLbl,
                 count_failedTurns_nnOut_userOut, count_failed_turns,
                 counter_failed_nnOut_tknLblIds)
     count_failed_turns += len({
-        failed_bchIdx_nnOutTknLblIdIdx_entityWrdLbl_userOut[0]
-        for failed_bchIdx_nnOutTknLblIdIdx_entityWrdLbl_userOut in
-        failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut
+        failed_bchIdx_nnOutTknLblIdIdx_entityLbl_userOut[0]
+        for failed_bchIdx_nnOutTknLblIdIdx_entityLbl_userOut in
+        failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut
     })
 
     bch_nnIn_tkns: List[List[str]] = []
@@ -128,7 +120,7 @@ def failed_nnOut_tknLblIds(
     bch_nnOut_tknLbls: List[List[str]] = []
     nnIn_tknIds_beginEnd_idx = (
         bch['nnIn_tknIds']['input_ids'] == 102).nonzero()
-    for bch_idx in range(len(bch_nnOut_entityWrdLbls)):
+    for bch_idx in range(len(bch_nnOut_entityLbls)):
         bch_nnIn_tkns.append([])
         bch_unseen_tkns_predictSet.append([])
         bch_tknLbls_True.append([])
@@ -143,32 +135,31 @@ def failed_nnOut_tknLblIds(
                 tokenizer.convert_ids_to_tokens(nnIn_tknId))
             if nnIn_tknId in dataset_meta['test-set unseen tokens']:
                 bch_unseen_tkns_predictSet[-1].append(bch_nnIn_tkns[-1][-1])
-            bch_tknLbls_True[-1].append(
-                dataset_meta['idx2tknLbl'][bch['tknLblIds'][bch_idx,
-                                                            nnIn_tknIds_idx]])
-            bch_nnOut_tknLbls[-1].append(dataset_meta['idx2tknLbl'][
+            bch_tknLbls_True[-1].append(dataset_meta['tknLblId2tknLbl'][
+                bch['tknLblIds'][bch_idx, nnIn_tknIds_idx]])
+            bch_nnOut_tknLbls[-1].append(dataset_meta['tknLblId2tknLbl'][
                 bch_nnOut_tknLblIds[bch_idx, nnIn_tknIds_idx]])
 
-    with failed_nnOut_tknLblIds.open('a') as file:
+    with failed_nnOut_tknLblIds_file.open('a') as file:
         prev_bch_idx: int = None
         bch_idx: int = None
         wrapper: textwrap.TextWrapper = textwrap.TextWrapper(
             width=80, initial_indent="", subsequent_indent=21 * " ")
         for bch_idx, nnOut_tknLblId_idx in (
-                failed_bchIdxs_nnOutTknLblIdIdxs_entityWrdLbls_userOut):
+                failed_bchIdxs_nnOutTknLblIdIdxs_entityLbls_userOut):
             # only FAILED bch_idx and nnOut_tknLblId_idx are considered
             index_of_first_SEP_plus1 = nnIn_tknIds_beginEnd_idx[bch_idx * 2,
                                                                 1] + 1
 
             if prev_bch_idx is not None and bch_idx != prev_bch_idx:
                 for strng in (
-                        f"entityWrdLbls_True = {' '.join(bch_entityWrdLbls_True[prev_bch_idx])}",
-                        f"nnOut_entityWrdLbls = {' '.join(bch_nnOut_entityWrdLbls[prev_bch_idx])}"
-                        if bch_nnOut_entityWrdLbls[prev_bch_idx] is not None
-                        else "nnOut_entityWrdLbls: None",
-                        f"Failed-nnOut_entityWrdLbls (entityWrdLbls_True, nnOut_entityWrdLbls): {', '.join(bch_failed_nnOut_entityWrdLbls[prev_bch_idx])}"
-                        if bch_failed_nnOut_entityWrdLbls[prev_bch_idx] else
-                        "Failed-nnOut_entityWrdLbls: None",
+                        f"entityLbls_True = {' '.join(bch_entityLbls_True[prev_bch_idx])}",
+                        f"nnOut_entityLbls = {' '.join(bch_nnOut_entityLbls[prev_bch_idx])}"
+                        if bch_nnOut_entityLbls[prev_bch_idx] is not None
+                        else "nnOut_entityLbls: None",
+                        f"Failed-nnOut_entityLbls (entityLbls_True, nnOut_entityLbls): {', '.join(bch_failed_nnOut_entityLbls[prev_bch_idx])}"
+                        if bch_failed_nnOut_entityLbls[prev_bch_idx] else
+                        "Failed-nnOut_entityLbls: None",
                         f"userIn_filtered_entityWrds = {' '.join(bch_userIn_filtered_entityWrds[prev_bch_idx])}"
                         if bch_userIn_filtered_entityWrds[prev_bch_idx]
                         is not None else "userIn_filtered_entityWrds: None",
@@ -219,13 +210,13 @@ def failed_nnOut_tknLblIds(
             [value for value in counter_failed_nnOut_tknLblIds.values()])
         assert bch_idx is not None
         for strng in (
-                f"entityWrdLbls_True = {' '.join(bch_entityWrdLbls_True[bch_idx])}",
-                f"nnOut_entityWrdLbls = {' '.join(bch_nnOut_entityWrdLbls[bch_idx])}"
-                if bch_nnOut_entityWrdLbls[bch_idx] is not None else
-                "nnOut_entityWrdLbls: None",
-                f"Failed-nnOut_entityWrdLbls (entityWrdLbls_True, nnOut_entityWrdLbls): {', '.join(bch_failed_nnOut_entityWrdLbls[bch_idx])}"
-                if bch_failed_nnOut_entityWrdLbls[bch_idx] else
-                "Failed-nnOut_entityWrdLbls: None",
+                f"entityLbls_True = {' '.join(bch_entityLbls_True[bch_idx])}",
+                f"nnOut_entityLbls = {' '.join(bch_nnOut_entityLbls[bch_idx])}"
+                if bch_nnOut_entityLbls[bch_idx] is not None else
+                "nnOut_entityLbls: None",
+                f"Failed-nnOut_entityLbls (entityLbls_True, nnOut_entityLbls): {', '.join(bch_failed_nnOut_entityLbls[bch_idx])}"
+                if bch_failed_nnOut_entityLbls[bch_idx] else
+                "Failed-nnOut_entityLbls: None",
                 f"userIn_filtered_entityWrds = {' '.join(bch_userIn_filtered_entityWrds[bch_idx])}"
                 if bch_userIn_filtered_entityWrds[bch_idx] is not None else
                 "userIn_filtered_entityWrds: None",
@@ -242,7 +233,7 @@ def failed_nnOut_tknLblIds(
             file.write("\n")
 
     return (count_total_turns, count_failed_nnOut_tknLblIds,
-            count_failedTurns_nnOut_entityWrdLbl,
+            count_failedTurns_nnOut_entityLbl,
             count_failedTurns_nnOut_userOut, count_failed_turns,
             counter_failed_nnOut_tknLblIds)
 
@@ -271,12 +262,12 @@ def prepare_metric(
                 ) == prev_firstTknOfWrd_idx:
                 continue  # ignore tknId that is not first-token-of-word
             prev_firstTknOfWrd_idx = firstTknOfWrd_idx
-            nnOut_tknLbl_True = dataset_meta['idx2tknLbl'][bch['tknLblIds'][
-                bch_idx, nnIn_tknIds_idx]]
+            nnOut_tknLbl_True = dataset_meta['tknLblId2tknLbl'][
+                bch['tknLblIds'][bch_idx, nnIn_tknIds_idx]]
             #assert nnOut_tknLbl_True != "T"
             assert nnOut_tknLbl_True[0] != "T"
             y_true[-1].append(nnOut_tknLbl_True)
-            nnOut_tknLbl = dataset_meta['idx2tknLbl'][bch_nnOut_tknLblIds[
+            nnOut_tknLbl = dataset_meta['tknLblId2tknLbl'][bch_nnOut_tknLblIds[
                 bch_idx, nnIn_tknIds_idx]]
             #if nnOut_tknLbl == "T":
             if nnOut_tknLbl[0] == "T":
@@ -300,7 +291,7 @@ def print_statistics(
     count_total_turns: int,
     count_failed_turns: int,
     count_failed_nnOut_tknLblIds: int,
-    count_failedTurns_nnOut_entityWrdLbl: int,
+    count_failedTurns_nnOut_entityLbl: int,
     count_failedTurns_nnOut_userOut: int,
     counter_failed_nnOut_tknLblIds: Dict[str, int],
     y_true: List[List[str]],
@@ -318,9 +309,9 @@ def print_statistics(
                 failed_turns = (f'# of failed turns = {count_failed_turns}')
                 failed_nnOut_tknLblIds = ('# of failed nnOut_tknLblIds = '
                                           f'{count_failed_nnOut_tknLblIds}')
-                failedTurns_nnOut_entityWrdLbl = (
-                    '# of turns of failed nnOut_entityWrdLbl = '
-                    f'{count_failedTurns_nnOut_entityWrdLbl}')
+                failedTurns_nnOut_entityLbl = (
+                    '# of turns of failed nnOut_entityLbl = '
+                    f'{count_failedTurns_nnOut_entityLbl}')
                 failedTurns_nnOut_userOut = (
                     '# of turns of failed nnOut_userOut = '
                     f'{count_failedTurns_nnOut_userOut}')
@@ -328,7 +319,7 @@ def print_statistics(
                         turns,
                         failed_turns,
                         failed_nnOut_tknLblIds,
-                        failedTurns_nnOut_entityWrdLbl,
+                        failedTurns_nnOut_entityLbl,
                         failedTurns_nnOut_userOut,
                 ):
                     print(strng)
@@ -348,8 +339,8 @@ def print_statistics(
                     print('None')
 
                 relevant_keys_of_dataset_meta = [
-                    'bch sizes', 'dataset splits', 'dataset lengths',
-                    'pandas data-frame file location'
+                    'bch sizes', 'dataset lengths',
+                    'pandas predict data-frame file location'
                 ]
                 for k, v in dataset_meta.items():
                     if k in relevant_keys_of_dataset_meta:
