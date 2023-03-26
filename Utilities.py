fde358a9f73af4eed48004e932f85570956795c1
vineetk1 <vineetchandhok@gmail.com>
2023-03-25 22:53:25 -0400

    name changes plus changes due to debugging

--- Utilities.py
+++ Utilities.py
@@ -7,7 +7,8 @@ from typing import List, Dict, Tuple, Union
 import string
 from enum import Enum
 import torch
-import data.synthetic_dataset as syntheticData
+import copy
+import generate_dataset.Synthetic_dataset as syntheticData
 
 logg = getLogger(__name__)
 
@@ -166,9 +167,10 @@ def userIn_filter_splitWords(userIn: str) -> List[str]:
             case "$":   # dollar
                 # begin (include + 2 new words) begin;
                 # begin2end (include + new word) begin;
-                # mid (exclude) mid_begin; mid2end (exclude + new word) begin;
+                # mid (exclude) mid_begin;
+                # mid2end (include + 2 new words) begin;
                 # mid_begin (exclude) mid_begin;
-                # mid_begin2end (exclude + new word) begin
+                # mid_begin2end (include + 2 two words) begin
                 match charPos_inUserInWrd:
                     case CharPos_inUserInWrd.BEGIN:
                         userIn2idx.append([char_idx, char_idx])
@@ -178,10 +180,12 @@ def userIn_filter_splitWords(userIn: str) -> List[str]:
                     case CharPos_inUserInWrd.MID2END:
                         userIn_wrd2Idx.append(char_idx-1)
                         userIn2idx.append(userIn_wrd2Idx)
+                        userIn2idx.append([char_idx, char_idx])
                         charPos_inUserInWrd = CharPos_inUserInWrd.BEGIN
                         userIn_wrd2Idx = []
                     case CharPos_inUserInWrd.MID_BEGIN2END:
                         userIn2idx.append(userIn_wrd2Idx)
+                        userIn2idx.append([char_idx, char_idx])
                         charPos_inUserInWrd = CharPos_inUserInWrd.BEGIN
                         userIn_wrd2Idx = []
                     case CharPos_inUserInWrd.MID:
@@ -276,13 +280,29 @@ def userIn_filter_splitWords(userIn: str) -> List[str]:
                 case "-":   # hyphen
                     try:
                         if idx_in_userIn2idx:
-                            float(userIn_filter_split[idx_in_userIn2idx-1])
+                            if userIn_filter_split[
+                               idx_in_userIn2idx-1] in syntheticData.all_units:
+                                if idx_in_userIn2idx-1:
+                                    float(userIn_filter_split[
+                                                        idx_in_userIn2idx-2])
+                                else:
+                                    remove_word_idxs.append(idx_in_userIn2idx)
+                                    continue
+                            else:
+                                float(userIn_filter_split[idx_in_userIn2idx-1])
                         else:
                             remove_word_idxs.append(idx_in_userIn2idx)
                             continue
                         if idx_in_userIn2idx < len(userIn_filter_split) - 1:
-                            if userIn_filter_split[idx_in_userIn2idx+1] == "$":
-                                pass
+                            if userIn_filter_split[
+                               idx_in_userIn2idx+1] in syntheticData.all_units:
+                                if idx_in_userIn2idx < len(
+                                                    userIn_filter_split) - 2:
+                                    float(
+                                      userIn_filter_split[idx_in_userIn2idx+2])
+                                else:
+                                    remove_word_idxs.append(idx_in_userIn2idx)
+                                    continue
                             else:
                                 float(userIn_filter_split[idx_in_userIn2idx+1])
                         else:
@@ -306,7 +326,7 @@ def tknLblIds2entity_wrds_lbls(
         bch_map_tknIdx2wrdIdx: List[List[int]],
         bch_userIn_filtered: List[List[str]],
         bch_nnOut_tknLblIds: torch.Tensor,
-        id2tknLbl: List[str],
+        tknLblId2tknLbl: List[str],
         DEBUG_bch_tknLblIds_True,
         DEBUG_tokenizer,
         ) -> Tuple[List[Union[List[str], None]], List[Union[List[str], None]]]:
@@ -320,12 +340,13 @@ def tknLblIds2entity_wrds_lbls(
                 (D_nnIn_tknIds_idx_beginEnd[bch_idx * 2, 1] + 1), (
                    D_nnIn_tknIds_idx_beginEnd[(bch_idx * 2) + 1, 1])):
             D_nnIn_tkn = DEBUG_tokenizer.convert_ids_to_tokens(bch_nnIn_tknIds[bch_idx][D_nnIn_tknIds_idx].item())
-            D_tknLbl_True = id2tknLbl[DEBUG_bch_tknLblIds_True[bch_idx, D_nnIn_tknIds_idx]]
-            D_nnOut_tknLbl = id2tknLbl[bch_nnOut_tknLblIds[bch_idx, D_nnIn_tknIds_idx]]
+            D_tknLbl_True = tknLblId2tknLbl[DEBUG_bch_tknLblIds_True[bch_idx, D_nnIn_tknIds_idx]]
+            D_nnOut_tknLbl = tknLblId2tknLbl[bch_nnOut_tknLblIds[bch_idx, D_nnIn_tknIds_idx]]
             D_userIn_filtered_wrd = bch_userIn_filtered[bch_idx][bch_map_tknIdx2wrdIdx[bch_idx][D_nnIn_tknIds_idx]]
             D_bch_associate[-1].append((D_nnIn_tknIds_idx, D_userIn_filtered_wrd, D_nnIn_tkn, D_tknLbl_True, D_nnOut_tknLbl))
     # ******************remove DEBUG code ending  here**********************
     # NOTE: Remove all ASSERTS from Production code of this function
+    #   Right now one assert is commented at: assert not nnOut_tknLbl == 'T'
     assert bch_nnOut_tknLblIds.shape == bch_nnIn_tknIds.shape
 
     bch_nnOut_entityWrdLbls: List[List[str]] = []
@@ -338,7 +359,7 @@ def tknLblIds2entity_wrds_lbls(
     # tknIds between two SEP belong to tknIds of words in
     # bch['userIn_filtered']
     nnIn_tknIds_idx_beginEnd: torch.Tensor = (bch_nnIn_tknIds == 102).nonzero()
-    tknId_of_O: int = id2tknLbl.index("O")
+    tknLblId_of_O: int = tknLblId2tknLbl.index("O")
 
     for bch_idx in range(bch_nnOut_tknLblIds.shape[0]):
         multipleWord_entity: str = ""
@@ -355,7 +376,7 @@ def tknLblIds2entity_wrds_lbls(
                  nnIn_tknIds_idx]) == prev_userIn_filtered_idx:
                 continue    # ignore tknId that is not first-token-of-the-word
             prev_userIn_filtered_idx = userIn_filtered_idx
-            if bch_nnOut_tknLblIds[bch_idx, nnIn_tknIds_idx] == tknId_of_O:
+            if bch_nnOut_tknLblIds[bch_idx, nnIn_tknIds_idx] == tknLblId_of_O:
                 if multipleWord_entity:  # previous multipleWord_entity
                     bch_userIn_filtered_entityWrds[-1].append(
                                                         multipleWord_entity)
@@ -363,7 +384,7 @@ def tknLblIds2entity_wrds_lbls(
                 prev_BIO = "O"   # next tkn is "B" or "O"
                 continue    # ignore tknId of "O"
 
-            nnOut_tknLbl = id2tknLbl[bch_nnOut_tknLblIds[
+            nnOut_tknLbl = tknLblId2tknLbl[bch_nnOut_tknLblIds[
                                                      bch_idx, nnIn_tknIds_idx]]
             if nnOut_tknLbl[-1] == ')':
                 # there MUST be an opening-parenthesis, else error
@@ -394,7 +415,8 @@ def tknLblIds2entity_wrds_lbls(
                            1) >= max_count_wrongPredictions_plus1):
                             break
                     else:
-                        multipleWord_entity = f"{multipleWord_entity} {entityWrd}"
+                        multipleWord_entity = (
+                                f"{multipleWord_entity} {entityWrd}")
                     prev_BIO = "I"    # next tkn is "B" or "I" or "O"
                 elif prev_BIO == "O":
                     count_wrongPredictions = max_count_wrongPredictions_plus1
@@ -405,7 +427,8 @@ def tknLblIds2entity_wrds_lbls(
                     if multipleWord_entity:  # previous multipleWord_entity
                         bch_userIn_filtered_entityWrds[-1].append(
                                                            multipleWord_entity)
-                    bch_nnOut_entityWrdLbls[-1].append(f"Changed_ItoB-{entityWrdLbl}")
+                    bch_nnOut_entityWrdLbls[-1].append(
+                            f"Changed_ItoB-{entityWrdLbl}")
                     multipleWord_entity = entityWrd
                     prev_BIO = "B"    # next tkn is "B" or "I" or "O"
                     if ((count_wrongPredictions := count_wrongPredictions + 1)
@@ -418,7 +441,7 @@ def tknLblIds2entity_wrds_lbls(
                     count_wrongPredictions = max_count_wrongPredictions_plus1
                     break
             else:
-                assert not nnOut_tknLbl == 'T'
+                #assert not nnOut_tknLbl == 'T'
                 count_wrongPredictions = max_count_wrongPredictions_plus1
                 break
         if multipleWord_entity:  # previous multipleWord_entity
@@ -439,16 +462,16 @@ def tknLblIds2entity_wrds_lbls(
 
 def userOut_init():
     userOut = {}
-    for carEntityWrdLbl in syntheticData.groupOf_carEntityWrdLbls:
-        # dict keys in same order as syntheticData.groupOf_carEntityWrdLbls
-        userOut[carEntityWrdLbl] = []
+    for carEntityLbl in syntheticData.carEntityLbls:
+        userOut[carEntityLbl] = []
     return userOut
 
 
 def generate_userOut(
-        bch_userOut: List[Dict[str, List[str]]],
+        bch_prevTrnUserOut: List[Dict[str, List[str]]],
         bch_userIn_filtered_entityWrds: List[List[str]],
         bch_entityWrdLbls: List[List[str]]) -> List[Dict[str, List[str]]]:
+    bch_userOut = copy.deepcopy(bch_prevTrnUserOut)
     assert len(bch_entityWrdLbls) == len(bch_userIn_filtered_entityWrds)
     assert len(bch_entityWrdLbls) == len(bch_userOut)
     # **** For Deployment: Code is written so Asserts, along with associated If
@@ -473,7 +496,7 @@ def generate_userOut(
             match entityWrdLbl := bch_entityWrdLbls[bch_idx][wrdLbl_idx]:
                 case entityWrdLbl if (
                         entityWrdLbl in syntheticData.
-                        groupOf_carEntityWrdLbls_with_nonNum_entityWrds):
+                        carEntityLbls_for_nonNumEntityWrds):
                     if carEntityNumsLbl:
                         if not carEntityNums:
                             assert False
@@ -486,7 +509,7 @@ def generate_userOut(
                     bch_userOut[bch_idx][entityWrdLbl].append(entityWrd)
                 case entityWrdLbl if ((
                         entityWrdLbl in syntheticData.
-                        groupOf_carEntityWrdLbls_with_Num_entityWrds)):
+                        carEntityLbls_for_numEntityWrds)):
                     if carEntityNumsLbl and carEntityNumsLbl != entityWrdLbl:
                         if not carEntityNums:
                             assert False
@@ -500,7 +523,7 @@ def generate_userOut(
                 case 'units_price' | 'units_mileage':
                     if not unit or (
                             unit in syntheticData.
-                            wrds_for_nonCar_entityWrdLbl[entityWrdLbl]):
+                            nonCarEntityLbls_mapTo_entityWrds[entityWrdLbl]):
                         unit = entityWrd
                         if cmd and len(carEntityNums) >= carEntityNumsNeeded:
                             transition(bch_userOut[bch_idx], cmd, unit,
@@ -510,7 +533,7 @@ def generate_userOut(
                             carEntityNumsLbl = ""
                     else:
                         # unit not in
-                        # syntheticData.wrds_for_nonCar_entityWrdLbl[entityWrdLbl])
+                        # syntheticData.nonCarEntityLbls_mapTo_entityWrds[entityWrdLbl])
                         if not (carEntityNumsLbl and carEntityNums):
                             assert False
                         if carEntityNumsLbl and carEntityNums:
@@ -523,8 +546,8 @@ def generate_userOut(
                         unit = entityWrd
                 case 'more' | 'less':
                     if cmd:
-                        if len(carEntityNums) < carEntityNumsNeeded:
-                            assert False
+                        #if len(carEntityNums) < carEntityNumsNeeded:
+                        #    assert False
                         if len(carEntityNums) == carEntityNumsNeeded:
                             # start of sentence-segment; less than $5000
                             transition(bch_userOut[bch_idx], cmd, unit,
@@ -565,8 +588,8 @@ def generate_userOut(
                         carEntityNumsLbl = ""
                 case 'range1':
                     if cmd:
-                        if len(carEntityNums) < carEntityNumsNeeded:
-                            assert False
+                        #if len(carEntityNums) < carEntityNumsNeeded:
+                        #    assert False
                         if len(carEntityNums) >= carEntityNumsNeeded:
                             transition(bch_userOut[bch_idx], cmd, unit,
                                        carEntityNums, carEntityNumsLbl)
@@ -580,8 +603,8 @@ def generate_userOut(
                 case 'range2':
                     # $2000 - $4000
                     if cmd:
-                        if len(carEntityNums) <= carEntityNumsNeeded:
-                            assert False
+                        #if len(carEntityNums) <= carEntityNumsNeeded:
+                        #    assert False
                         if len(carEntityNums) > carEntityNumsNeeded:
                             transition(bch_userOut[bch_idx], cmd, unit,
                                        carEntityNums[:-1], carEntityNumsLbl)
@@ -601,7 +624,7 @@ def generate_userOut(
                         cmd, carEntityNumsNeeded = entityWrdLbl, 2
                         carEntityNums = [carEntityNums[-1]]
                     else:   # not carEntityNums:
-                        assert False
+                        #assert False
                         # bad userIn-seg; throw previous collected info
                         # plus this one
                         cmd, carEntityNumsNeeded, unit = "", None, ""
@@ -609,8 +632,8 @@ def generate_userOut(
                         carEntityNumsLbl = ""
                 case 'remove':
                     if cmd:
-                        if len(carEntityNums) < carEntityNumsNeeded:
-                            assert False
+                        #if len(carEntityNums) < carEntityNumsNeeded:
+                        #    assert False
                         if len(carEntityNums) >= carEntityNumsNeeded:
                             transition(bch_userOut[bch_idx], cmd, unit,
                                        carEntityNums, carEntityNumsLbl)
@@ -633,14 +656,15 @@ def generate_userOut(
                             # remove everything
                             for key in bch_userOut[bch_idx].keys():
                                 bch_userOut[bch_idx][key].clear()
-                        case entityWrdLbl if (entityWrdLbl in syntheticData.
-                                              groupOf_carEntityWrdLbls):
+                        case 'carEntityLbl':
                             # remove brand                 delete brands
                             assert (entityWrd in syntheticData.
-                                    groupOf_carEntityWrdLbls) or (
-                                            entityWrd[:-1] in syntheticData.
-                                            groupOf_carEntityWrdLbls)
-                            bch_userOut[bch_idx][entityWrdLbl].clear()
+                                    nonCarEntityLbls_mapTo_entityWrds[
+                                        entityWrdLbl])
+                            if entityWrd[-1] == "s":
+                                bch_userOut[bch_idx][entityWrd[:-1]].clear()
+                            else:
+                                bch_userOut[bch_idx][entityWrd].clear()
                         case _:
                             assert False
                             wrdLbl_idx -= 1
@@ -692,8 +716,8 @@ def transition(userOut: Dict[str, List[str]], cmd: str, unit: str,
             assert False
 
 
-def userOut2history(userOut: Dict[str, List[str]]) -> List[str]:
+def prevTrnUserOut2history(prevTrnUserOut: Dict[str, List[str]]) -> List[str]:
     history: List[str] = []
-    for values in userOut.values():
+    for values in prevTrnUserOut.values():
         history.extend(values)
     return history
