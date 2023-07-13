527d07b2fdffe30f0e159865c2a67df8305df386
vineetk1 <vineetchandhok@gmail.com>
2023-07-13 18:51:11 -0400

    add new examples and deleted bad ones

--- generate_dataset/Synthetic_dataset.py
+++ generate_dataset/Synthetic_dataset.py
@@ -61,6 +61,21 @@ Some notes:
 -----------
 + Instead of using a -ve, a previous range can be removed by
     mentioning a new range
+
+NN must learn when to label or not-label the entityWrds:
+--------------------------------------------------------
+Add to segments certain entityWrds (e.g. between, to, through) that the NN must learn to NOT label. Example:
+"i have to tell you that <year><> <range2><> <year><>   <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><>  <units_price_$><> <price><>   <range2><-> <price><>  is between a few cars i want through"
+In the above sentence, the NN must NOT label the "to", "between", "through"
+
+
+Segments that are NOT ALLOWED:
+------------------------------
+" <price><>   <units_price_$><>  <range2><-> <price><> ",
+" <price><>   <range2><->  <units_price_$><> <price><> ",
+Reason: if "units_price_$" is "dollar" then its typo could become "dolkar". The
+function Utilities.py: userIn_filter_splitWords(userIn) removes the hyphen
+because "dolkar" is neither a number or a unit
 """
 import random
 
@@ -68,32 +83,86 @@ PLACEHOLDER_ID_START = "<"
 PLACEHOLDER_ID_END = ">"
 
 train_sentences = (
+    # user should normally type as follows
+    " <color><> <year><> <brand><> <model><>  <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><>   <range1><>   <units_price_$><> <price><> and <price><>  ",
+    " <model><>  <year><> <range2><> <year><>   <color><>    <less><> than  <price><> <units_price_$><> and  <less><> than  <mileage><>   <units_mileage_mi><> ",
+    " <color><>  <more><>  than <year><>  <model><> <brand><>   <units_price_$><> <price><>   <range2><-> <price><>  <mileage><>   <range2><->  <units_mileage_mi><mi> <mileage><>  ",
+    "<brand><> <less><>  <year><>   <color><> <model><>   <range1><>  <mileage><>  <units_mileage_mi><> and <mileage><>    <range1><>   <price><> <price><>  <units_price_$><> ",
+
+    # NN must sometimes NOT label entityWrds; usually (but not always) when they are between two "O" words
+    "give <color><> through <brand><>  ",  # In Predict, NN must not label "through" as range2 label
+    " run it <year><> <range2><> <year><> and to the ground  <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><> either higher or lower it don't matter  <units_price_$><> <price><>  <range2><-> <price><> because little is known",
+    "between the world of  <year><> but larger than  <color><> and smaller than <brand><> lies <price><>  <range2><-> <price><> <units_price_$><> ",
+    "it's more  <brand><> and above <model><> but through below the price that is not right <less><> than  <units_price_$><> <price><>",
+    " under the <model><>  but over the  <brand><> in the range of  <color><> for <less><> than  <mileage><>   <units_mileage_mi><> ",
+    "your mileage and $ or dollar is less but prices are greater so I will give <price><> <units_price_$><>",
+    "the range of mileages and dollars is above or below but i say <units_price_$><> <price><>   <range2><-> <price><> which is neither greater or little to whatcha say <mileage><>   <units_mileage_mi><>",
+
+    # NN must not always label 1970 - 2024 as belonging to year
+    "<color><> <year><1992> <brand><> <price><1992> <range2><> <price><2020> <units_price_$><>  with <less><less> than <mileage><1992> <units_mileage_mi><miles> ",
+    "with <mileage><2016> <units_mileage_mi><miles> and <price><2016> <units_price_$><> <year><2016> or <more><more>",
+    " <brand><> <year><1975> <units_price_$><> <price><1975> <more><more> <mileage><1975> <units_mileage_mi><miles>",
+    "<year><1982> <model><>  <units_price_$><> <price><1994> <mileage><1999> <units_mileage_mi><miles> <more><more> ",
+
+    # miscellaneous
     "<year><> <model><>",
     "<year><> <brand><>",
     "<year><> <color><> <model><>",
     "<year><> <color><> <brand><>",
 )
 val_sentences = (
+    # user should normally type as follows
+    "<mileage><>   <range2><-> <mileage><>  <units_mileage_mi><>   <range1><>   <units_price_$><> <price><> and <price><>  <year><> <color><> <brand><> <model><>  ",
+    "  <year><> <range2><> <year><>   <color><>    <less><> than  <units_price_$><> <price><> <model><>   <less><> than  <mileage><>   <units_mileage_mi><> ",
+    " <color><>  <more><>  than <year><>  <model><>  <units_price_$><> <price><>   <range2><-> <price><>  <mileage><>   <range2><->  <units_mileage_mi><mi> <mileage><>   <brand><> ",
+    "<brand><>  <color><> <model><>   <range1><>  <mileage><>  and <mileage><>  <units_mileage_mi><>   <range1><>   <units_price_$><> <price><> <price><>   <less><>  <year><> ",
+
+    # NN must sometimes NOT label entityWrds; usually (but not always) when they are between two "O" words
+    "<color><> to <model><> and in between the $ and still there are <less><> than  <units_price_$><> <price><> ",
+    "i have to tell  that larger than dollars are  <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><> but they are not above this  <year><> <range2><> <year><> and the range is incomplete  <units_price_$><> <price><>   <range2><-> <price><> ",
+    "smaller than   <year><> but higher than the usual <price><> <units_price_$><> <color><> and below  <brand><> ",
+    "  <brand><> but through i want <less><> than <units_price_$><>   <price><> regardless of greater prices",
+    " mileages are under <model><>  when little is known of <price><>   <range2><-> <price><>  <units_price_$><> and less the better for  <mileage><>   <units_mileage_mi><> ",
+    "<price><> <units_price_$><> is the price i'll pay for <brand><> which is more  ",
+    "over the years the  <price><>   <range2><-> <price><> <units_price_$><> is not lower in dollar for <color><> without the mileage",
+
+    # NN must not always label 1970 - 2024 as belonging to year
+    "<model><> <brand><>  <year><2008> <price><2008> <range2><> <price><2008> <units_price_$><>  with  <mileage><2008> <units_mileage_mi><miles> <more><more> ",
+    "with <mileage><2016> <units_mileage_mi><miles> and <price><2016> <units_price_$><> <year><2016> or <more><more>",
+    " <color><> <year><1975> <units_price_$><> <price><1975> <more><more> <mileage><1975> <units_mileage_mi><miles>",
+    "<year><2010> <model><> <mileage><2003> <units_mileage_mi><miles>   <less><less> than  <units_price_$><> <price><1987>",
+
+    # miscellaneous
     "<year><> <model><>",
     "<year><> <brand><>",
     "<year><> <color><> <model><>",
     "<year><> <color><> <brand><>",
 )
 test_sentences = (
-    "<year><2020> <model><camry> with <less><less> than <mileage><2020> <units_mileage_mi><miles> <color><> <price><2015> <range2><> <price><2020> <units_price_$><>",
-    "<year><2020> <model><camry> with <less><less> than <mileage><2020> <units_mileage_mi><miles> <color><> <range1><between> <price><2015> <range2><to> <price><2020> <units_price_$><>",
-    "<year><> <range2><> <year><> <color><> <model><> <brand><>",
-    "<year><2015> or <more><more> with <mileage><2015> <units_mileage_mi><miles> and <price><2015> <units_price_$><>",
-    "<year><2023> <units_price_$><> <price><2015> <more><more> <mileage><2015> <units_mileage_mi><miles>",
-    "<year><2040> <units_price_$><> <price><2015> <mileage><2015> <more><more> <units_mileage_mi><miles>",
-    "<range1><between> <units_price_$><> <price><> and <price><>",
-    "<range1><between> <units_price_$><> <price><> <range2><to> <price><>",
+    # user should normally type as follows
+    "<year><> <color><> <brand><> <model><> <less><> than  <units_price_$><> <price><> and  <less><> than  <mileage><>   <units_mileage_mi><> ",
+    " <color><>  <more><>  than <year><>  <model><> <brand><>  <range1><>  <mileage><>  <units_mileage_mi><> and <mileage><>    <range1><>   <units_price_$><> <price><> <price><>  ",
+    "<brand><> <less><>  <year><>   <color><> <model><>   <units_price_$><> <price><>   <range2><-> <price><>  <mileage><>   <range2><->  <units_mileage_mi><mi> <mileage><> ",
+    " <model><>  <year><> <range2><> <year><>   <color><>      <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><>   <range1><>   <units_price_$><> <price><> and <price><> ",
+
+    # NN must sometimes NOT label entityWrds; usually (but not always) when they are between two "O" words
+    "these dollars and mileages are not worth <brand><> through <brand><>  ",  # In Predict, NN must not label "through" as range2 label
+    "i have to tell  that $ is not <year><> <range2><> <year><>   <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><>  <units_price_$><> <price><>   <range2><-> <price><>  is between a few cars i want through <model><> ",
+    "more or less it is dollars that count  <year><> but larger than  <color><> and smaller than <brand><> ",
+    "it's between  <brand><> and <brand><> but through and through i want <less><> than  <units_price_$><> <price><>",
+    " <model><>  is under  but  <brand><> is over  <color><> for <less><> than  <mileage><>   <units_mileage_mi><> ",
+    "your price is higher but mileage is lower so I will give <price><> <units_price_$><>",
+    "the range of prices is above or below but i say <price><>   <range2><-> <price><> <units_price_$><> which is neither greater or little",
+
+    # NN must not always label 1970 - 2024 as belonging to year
+    "<year><2020> <model><> with <less><less> than <mileage><2020> <units_mileage_mi><miles> <color><> <price><2015> <range2><> <price><2020> <units_price_$><>",
+    "with <mileage><2015> <units_mileage_mi><miles> <year><2015> or <more><more> and <price><2015> <units_price_$><>",
+    "<year><2023> <units_price_$><> <price><2023> <more><more> <mileage><2023> <units_mileage_mi><miles>",
+    "<year><2010> <units_price_$><> <price><2015> <mileage><2017> <units_mileage_mi><miles> <more><more> ",
+
+    # miscellaneous
     "<units_price_$><$> <price><> or <units_price_$><$> <price><> <range2><-> <price><>",
-    "<units_price_$><$> <price><> or <price><> <range2><-> <units_price_$><$> <price><>",
-    "i want <brand><> through <brand><>  ",  # In Predict, NN must not label "through" as range2 label
-    "  <mileage><>  <units_mileage_mi><> <range2><-> <mileage><> ",
     "  <range1><>  <price><> and <price><>  price ",
-    "<more><more> than price <price><5000> and <less><less> than <price><6000> price",
 )
 full_sentences = {
     "train": train_sentences,
@@ -246,18 +315,18 @@ price_segments = (
     "what is 'the' price of \"rice\" in china  ",
 
     # " <price><>",
-    "i will pay <units_price_$><$> <price><> for car",
-    " <units_price_$><$> <price><> is the way to go ",
     "<units_price_$><$> <price><>",
-    "<price><> <units_price_$><> ",
-    "<units_price_$><$> <price><> for this car",
-    "how about <price><> <units_price_$><> ",
+    "<price><> <units_price_$><dollars>",
+    "<units_price_$><> <price><> for car",
+    "i will pay <price><> <units_price_$><>",
+    "me thinks <units_price_$><> <price><> is the way to go ",
+    "how about <price><> <units_price_$><> for this junk",
     "<units_price_$><$> <price><> <price><> <units_price_$><dollars> ",
 
     # "  <price><> <range2><-> <price><> ",
     " <units_price_$><> <price><>   <range2><-> <price><> ",
-    " <price><>   <units_price_$><>  <range2><-> <price><> ",
-    " <price><>   <range2><->  <units_price_$><> <price><> ",
+    " <price><>   <units_price_$><$>  <range2><-> <price><> ",
+    " <price><>   <range2><->  <units_price_$><$> <price><> ",
     " <price><>   <range2><-> <price><>  <units_price_$><> ",
     " <units_price_$><> <price><>   <range2><-> <price><> as an example",
     " my car should be  <price><>   <range2><-> <price><> <units_price_$><>",
@@ -308,8 +377,8 @@ mileage_segments = (
 
     # "  <mileage><> <range2><-> <mileage><> ",
     " <units_mileage_mi><> <mileage><>   <range2><-> <mileage><> ",
-    " <mileage><>   <units_mileage_mi><>  <range2><-> <mileage><> ",
-    " <mileage><>   <range2><->  <units_mileage_mi><> <mileage><> ",
+    " <mileage><>   <units_mileage_mi><mi>  <range2><-> <mileage><> ",
+    " <mileage><>   <range2><->  <units_mileage_mi><mi> <mileage><> ",
     " <mileage><>   <range2><-> <mileage><>  <units_mileage_mi><> ",
     " <units_mileage_mi><> <mileage><>   <range2><-> <mileage><> will work for me",
     " my car should be  <mileage><>   <range2><-> <mileage><> <units_mileage_mi><>",
@@ -359,8 +428,6 @@ year_segments = (
     # " <year><> <range2><-> <year><>  ",
     " <year><> <range2><> <year><>  ",
     " year <year><> <range2><> <year><>  ",
-    "  <year><> year <range2><> <year><>  ",
-    "  <year><> <range2><>  year <year><>  ",
     " <year><> <range2><> <year><> years ",
     " for this reason <year><> <range2><> <year><> are the best ",
 
@@ -443,8 +510,8 @@ entityLbls_mapTo_segments = {
 
 cmdsLbls = {
     "restore": ("restore", "retrieve", "retreive",),
-    "more": ("over", "more", "high", "above", "larger", "greater"),
-    "less": ("under", "less", "low", "below", "smaller", "little"),
+    "more": ("over", "more", "higher", "above", "larger", "greater"),
+    "less": ("under", "less", "lower", "below", "smaller", "little"),
     "range1": ("between", "range"),  # e.g. between $5000 and $7000
     "range2": ("-", "to", "through"),  # e.g. $5000 - $7000
     "remove": ("remove", "delete"),
