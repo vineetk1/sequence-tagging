2864131511fedfa6c2c57b7340f52959d477ca7e
vineetk1 <vineetchandhok@gmail.com>
2023-09-25 15:35:05 -0400

    added probabilities for each segment-name

--- generate_dataset/Segments2Sentence.py
+++ generate_dataset/Segments2Sentence.py
@@ -6,7 +6,7 @@ from logging import getLogger
 from typing import List, Dict, Union
 import random
 from generate_dataset.Synthetic_dataset import (full_sentences,
-                                                entityLbls_mapTo_segments)
+                                                segments_weights)
 
 logg = getLogger(__name__)
 
@@ -24,8 +24,8 @@ class Segments2Sentence():
         self.segmentsInfo_per_entityLbl: Dict[str, Dict[str,
                                                         Union[List[str], int,
                                                               bool]]] = {}
-        for entityLbl, segments in entityLbls_mapTo_segments.items():
-            segments = list(segments)
+        for entityLbl, segs_wgts in segments_weights.items():
+            segments = list(segs_wgts["segments"])
             random.shuffle(segments)
             self.segmentsInfo_per_entityLbl[entityLbl] = {
                 'segs': segments,
@@ -34,6 +34,8 @@ class Segments2Sentence():
             }
 
     def get_sentence(self, segs_per_sentence=None) -> str:
+        if not self.full_sentences['sentences']:
+            self.full_sentences['sentences_used'] = True
         if not self.full_sentences['sentences_used']:
             sentence = self.full_sentences['sentences'][
                 self.full_sentences['sentence_idx']]
@@ -43,20 +45,31 @@ class Segments2Sentence():
             else:
                 self.full_sentences['sentence_idx'] += 1
             return sentence
-        segments_per_sentence = (
-            # input to model MUST be less than the max # of tokens allowed; too
-            # many segments in sentence could exceed max allowed
-            random.randint(1, 5)
-            #random.randint(1, len(self.segmentsInfo_per_entityLbl))
-            if segs_per_sentence is None else segs_per_sentence)
-        random_entityLbls_of_segmentsInfo_per_entityLbl = random.choices(
-            list(self.segmentsInfo_per_entityLbl.keys()),
-            k=segments_per_sentence)
-        sentence = ""
-        for entityLbl in random_entityLbls_of_segmentsInfo_per_entityLbl:
+
+        segments_per_sentence = random.randint(
+            1, len(self.segmentsInfo_per_entityLbl))
+        seg_names: List[str] = list(segments_weights.keys())
+        weights: List[int] = []
+        for seg_wgt in segments_weights.values():
+            weights.append(seg_wgt["weight"])
+        sentence: str = ""
+        segmentParts_count: int = 0
+        while segments_per_sentence:
+            entityLbl = random.choices(seg_names, weights=weights, k=1)[0]
             segment = self.segmentsInfo_per_entityLbl[entityLbl]['segs'][
                 self.segmentsInfo_per_entityLbl[entityLbl]['seg_idx']]
-            sentence = f"{sentence}{' ' if sentence else ''}{segment}"
+            # if tokenizer.model_max_length = 512 then user_in <= 100 words
+            # Assume each segment-part (e.g. <brand><> has 1 word,
+            # <multilabel><> has 2 words, <other><> has upto 3 words) generates
+            # an average of 1.5 words, then there must be a max of 67 (100/1.5)
+            # segment-parts in a sentence
+            seg_parts = len(segment.split())
+            assert seg_parts <= 67, "More than 67 segment-parts in a segment"
+            if segmentParts_count + seg_parts <= 67:
+                sentence = f"{sentence}{' ' if sentence else ''}{segment}"
+                segmentParts_count = segmentParts_count + seg_parts
+            else:
+                break
             if self.segmentsInfo_per_entityLbl[entityLbl]['seg_idx'] == (
                     len(self.segmentsInfo_per_entityLbl[entityLbl]['segs']) -
                     1):
@@ -66,6 +79,7 @@ class Segments2Sentence():
                     self.segmentsInfo_per_entityLbl[entityLbl]['segs'])
             else:
                 self.segmentsInfo_per_entityLbl[entityLbl]['seg_idx'] += 1
+            segments_per_sentence -= 1
         return sentence
 
     def get_segment(self) -> str:
@@ -86,7 +100,7 @@ class Segments2Sentence():
             segmentsInfo['segs_used'] += 1
 
     def all_segments_done(self) -> bool:
-        numOfTimes_segs_used = 1000
+        numOfTimes_segs_used = 300
         segs_used: bool = True
         for segmentsInfo in self.segmentsInfo_per_entityLbl.values():
             segs_used = segs_used and (segmentsInfo['segs_used'] >=
